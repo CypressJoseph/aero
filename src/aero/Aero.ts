4eb4af3c04043c464942d23906ffc595206b46c6
Joseph Weissman <joseph@cypress.io>
2020-05-20 15:17:57 -0400

    refactor a bit

--- src/aero/Aero.ts
+++ src/aero/Aero.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-constructor */
 
-import { Observable, Observer } from 'rxjs'
+import { Observable } from 'rxjs'
 import { filter } from 'rxjs/operators'
 import { toCamel } from './util'
 
@@ -24,27 +24,29 @@ type StoryTemplate = new () => Story
 export class Aero<Event extends AbstractEvent> {
   constructor (public observable: Observable<Event>) {}
 
-  fly (pilot: Observer<Event>) { this.observable.subscribe(pilot) }
-
   play (...StoryKinds: StoryTemplate[]) {
     for (const StoryKind of StoryKinds) {
-      const story = new StoryKind()
-      const matchesBeginning = filter((e: Event) => e.kind === story.startsWith)
-      const handleStoryEvent = (initialEvent: Event) => {
-        const matchesElements = filter((e: Event) => story.correlatedOn.every(
-          (correlated: string) => (initialEvent as any)[correlated] === (e as any)[correlated]
-        ))
-        const journey: any = new StoryKind()
-        const ctx = story.correlatedOn.map((attr: string) => [attr, (initialEvent as any)[attr]])
-        journey.context = Object.fromEntries(ctx)
-        this.observable.pipe(matchesElements).forEach(event => {
-          const journeyAction: string = toCamel(event.kind.split(':')[1])
-          const eventHandler: Function = journey[journeyAction]
-          const missingEventHandler = `warning: missing story event handler for ${event.kind}, e.g.: \n\n    class ${StoryKind.name} { ${journeyAction}() {...} }`
-          if (eventHandler) { eventHandler.call(journey, event) } else { journey.log(missingEventHandler) }
-        })
-      }
-      this.observable.pipe(matchesBeginning).forEach(handleStoryEvent)
+      const modelStory = new StoryKind()
+      const matchingInitial = filter((e: Event) => e.kind === modelStory.startsWith)
+      this.observable.pipe(matchingInitial).forEach(this.relate(StoryKind))
+    }
+  }
+
+  private relate (StoryKind: StoryTemplate) {
+    return (initialEvent: Event) => {
+      const story: Story = new StoryKind()
+      const { correlatedOn: correlate } = story
+      const correlatedEvents = filter((e: Event) => correlate.every((attribute: string) =>
+        (initialEvent as any)[attribute] === (e as any)[attribute]
+      ))
+      const ctx = correlate.map((attr: string) => [attr, (initialEvent as any)[attr]])
+      story.context = Object.fromEntries(ctx)
+      this.observable.pipe(correlatedEvents).forEach(event => {
+        const journeyAction: string = toCamel(event.kind.split(':')[1])
+        const eventHandler: Function = (story as any)[journeyAction]
+        const missingEventHandler = `warning: missing story event handler for ${event.kind}, e.g.: \n\n    class ${StoryKind.name} { ${journeyAction}() {...} }`
+        if (eventHandler) { eventHandler.call(story, event) } else { console.warn(missingEventHandler) }
+      })
     }
   }
 }
